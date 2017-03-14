ce11dbd8b33c785feb2aab4e07080a9f4b33780c
Appveyor Worker <>
2017-03-14 03:38:31 +0000

    Appveyor: Autoupdate documentation for 6899374578f8de72dae72fcdd0cbfa304ac856cd

--- doc/implementors/std/error/trait.Error.js
+++ doc/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["schannel"] = ["impl&lt;S:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/any/trait.Any.html' title='core::any::Any'>Any</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='schannel/tls_stream/enum.HandshakeError.html' title='schannel::tls_stream::HandshakeError'>HandshakeError</a>&lt;S&gt;",];
+implementors["schannel"] = ["impl&lt;S:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/any/trait.Any.html\" title=\"trait core::any::Any\">Any</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"schannel/tls_stream/enum.HandshakeError.html\" title=\"enum schannel::tls_stream::HandshakeError\">HandshakeError</a>&lt;S&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/std/io/trait.Write.js
+++ doc/implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["schannel"] = ["impl&lt;S&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='schannel/tls_stream/struct.TlsStream.html' title='schannel::tls_stream::TlsStream'>TlsStream</a>&lt;S&gt; <span class='where'>where S: <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a></span>",];
+implementors["schannel"] = ["impl&lt;S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"schannel/tls_stream/struct.TlsStream.html\" title=\"struct schannel::tls_stream::TlsStream\">TlsStream</a>&lt;S&gt; <span class=\"where fmt-newline\">where S: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
